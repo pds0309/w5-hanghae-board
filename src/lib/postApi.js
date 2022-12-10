2c2af29ceae024cb48fc28e884a849be06cdf4d0
pds0309 <76927397+pds0309@users.noreply.github.com>
2022-12-10 17:46:22 +0900

    getPosts api 경로 이동

--- src/lib/postApi.js
+++ src/lib/postApi.js
@@ -14,6 +14,18 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 //   }
 // );
 
+export const __getPosts = createAsyncThunk(
+  "getTodos",
+  async (payload, thunkAPI) => {
+    try {
+      const data = await axios.get("http://localhost:3001/posts");
+      return thunkAPI.fulfillWithValue(data.data);
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e);
+    }
+  }
+);
+
 export const __getPostById = createAsyncThunk(
   "getPostById",
   async (payload, thunkAPI) => {
