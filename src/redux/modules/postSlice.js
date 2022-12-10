987833cad8709c269cad7dc7238d5949f8019e48
Seongbae94 <seongbaelee944@gmail.com>
2022-12-10 16:13:27 +0900

    메인 페이지 1,2번

--- src/redux/modules/postSlice.js
+++ src/redux/modules/postSlice.js
@@ -1,7 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const __getPosts = createAsyncThunk(
+  "getTodos",
+  async (payload, thunkAPI) => {
+    try {
+      const data = await axios.get("http://localhost:3001/posts");
+      return thunkAPI.fulfillWithValue(data.data);
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e);
+    }
+  }
+);
 
 const initialState = {
   posts: [],
+  isLoading: false,
+  error: null,
 };
 
 export const postSlice = createSlice({
@@ -9,6 +24,19 @@ export const postSlice = createSlice({
   initialState,
   reducers: {
     // TODO: 액션 정의
+  },
+  extraReducers: {
+    [__getPosts.pending]: (state) => {
+      state.isLoading = true; // 네트워크 요청이 시작되면 로딩상태를 true로 변경합니다.
+    },
+    [__getPosts.fulfilled]: (state, action) => {
+      state.isLoading = false; // 네트워크 요청이 끝났으니, false로 변경합니다.
+      state.posts = action.payload; // Store에 있는 todos에 서버에서 가져온 todos를 넣습니다.
+    },
+    [__getPosts.rejected]: (state, action) => {
+      state.isLoading = false; // 에러가 발생했지만, 네트워크 요청이 끝났으니, false로 변경합니다.
+      state.error = action.payload; // catch 된 error 객체를 state.error에 넣습니다.
+    },
   },
 });
 
