ae933acad7807bbc13a809b18bf07eb5b01132fc
Seongbae94 <seongbaelee944@gmail.com>
2022-12-13 00:14:35 +0900

    axios instance 업데이트/ API 서버 URL 코드 숨김

--- src/components/instance/axiosCommentInstance.js
+++ src/components/instance/axiosCommentInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3001/comments",
+  baseURL: process.env.REACT_APP_URL + "/comments",
 });
 
 export default instance;
